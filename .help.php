336d82df796d1362a63f7523775c769cb977d114
sanikdah <michaelfgarofalo@gmail.com>
2021-07-05 23:50:21 -0700

    Update .help.php and .stylesheet.css with the newest code
    Changed .help.css to be much more complete, also changed .stylesheet.css to  potentially help with the textbox on .help.css, no clue if it really helps or not, needs more testing.

--- .help.php
+++ .help.php
@@ -11,4 +11,12 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://nas/";
         <a class="nav" href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a>  <! Hyperlink to Main-Page.php, the main page >
         <br>
         <br>
-	<p>If something doesn't work correctly on the website, feel free to email me at michaelfgarofalo@gmail.com, preferably with screenshots of the issue.  If you want to suggest something, title your email "Suggestion for the website" or at least something simillar.
+	<p>If something doesn't work correctly on the website, feel free to email me at michaelfgarofalo@gmail.com, preferably with screenshots of the issue.  If you want to suggest something, title your email "Suggestion for the website" or at least something simillar.</p>
+	<input type="radio" name="Suggest/Help"> Suggestion
+	<input type="radio" name="Suggest/Help"> Support
+	<form>
+	  <input style="height:500px;width:1400px;font-size:14pt;" textarea rows="2" cols="25"</textarea></input>  <! This is currently very messed up, while the text field DOES work, the text is stuck in the center vertically with only 1 line of text >
+	</form>
+	<input type="submit" name="Submit" style="background: teal;height: 40px;width: 120px">
+    </body>
+

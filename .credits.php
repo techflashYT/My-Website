4a15761f8c71c7f835a104623709bc7a1d86020d
sanikdah <michaelfgarofalo@gmail.com>
2021-07-06 00:33:54 -0700

    Update files
    Update any files that I changed that may not have been updated on the GitHub Repo.

--- .credits.php
+++ .credits.php
@@ -12,6 +12,8 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://nas/";
 	<br>
 	<br>
 	<p>Me: Creator, hoster</p>
-	<p>"BlueToonYoshi": Helped with the images on the "Useful Links" section</p>
+	<p>"BlueToonYoshi": Helped with the images on the "Useful Links" section.</p>
 	<p>"Void B⃢ot", "TheShadowEevee", "ping for so-so wii help | snare" "avane": helped a lot with the coding and setting up of the web server itself, a major thanks to these guys.</p>
 	<p>"nezzie": helped a lot with setting up PHP and setting up the switcher code, the code that switches the links if you are on my LAN, so that nobody gets dead links, big thanks.</p>
+	<p>"HP ⚡ kosarials": helped a lot with the "Suggestions and Support" section, they're the one that (will have hopefully by tomorrow) set up the emailing part!</p>
+    </body>

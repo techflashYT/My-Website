e0b4690bddac44d3b71893d63150f4b86c002219
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 13:06:45 -0700

    Make sure that everybody knows of Void B⃢ot's failure to get access for files working how I wanted it to.

--- .credits.php
+++ .credits.php
@@ -18,4 +18,5 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://nas/";
     </br></br>"nezzie": helped a lot with setting up PHP and setting up the switcher code, the code that switches the links if you are on my LAN, so that nobody gets dead links, big thanks to you.
     </br></br>"HP ⚡ kosarials": helped a lot with the "Suggestions and Support" section, they're the one that (will have hopefully by tomorrow) set up the emailing part!
     </br></br>"oscie": made the website look 1000x better by adding a stylesheet.
+    </br></br>"Void B⃢ot": Tried to help with access but failed.
     </body>

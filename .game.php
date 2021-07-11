d4e2a0ac1598fb3c8cf1cc4d9dc5ae9fb66b54f1
sanikdah <michaelfgarofalo@gmail.com>
2021-07-10 17:22:13 -0700

    Update .game.php

--- .game.php
+++ .game.php
@@ -10,6 +10,6 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
         <a href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a>
         <br>
         <iframe width="1050" height="812" src="https://www.crazygames.com/embed/bitcoiner" frameborder="0" allowfullscreen></iframe>
-        <p>Bitcoiner is a game all about mining bitcoin!  You start off with basic machines, no cooling and a small room, as you get more bitcoins, you can upgrade all of those things!  Can you get to 1 million bitcoin?</p>
+        <?php echo "Bitcoiner is a game all about mining bitcoin!  </br> You start off with basic machines, no cooling and a small room. </br> As you get more bitcoins, you can upgrade all of those things!  </br> Can you get to 1 million bitcoin?"; ?>
     </body>
 </html>

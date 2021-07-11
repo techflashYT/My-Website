4ba0fb52c19cde11f8affd1ebb2e8911f707fb69
sanikdah <michaelfgarofalo@gmail.com>
2021-07-10 22:31:37 -0700

    Update .aboutme.php to have formatting that is up to date with my current standards.

--- .aboutme.php
+++ .aboutme.php
@@ -7,17 +7,13 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://nas/";
         <title>Welcome to my page!</title>
     </head>
     <body>
-        <br>
+        </br>
         <a class="nav" href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a>  <! Hyperlink to Main-Page.php, the main page >
-        <br>
-        <p>This will be all about me in a bit!!<p>
-        <br>
-        <br>
-        <img style="height: 5%;" src=".logos/.Discord-transparent.png" alt="Discord Logo, why are you reading this text.">
-        <a href="https://discord.gg/cPdSqvcXc9">My Discord Server!</a>  <! Hyperlink to my Discord Server >
-        <br>
-	<br>
-	<img style="height: 4.5%;" src=".logos/.Youtube-Transparent.png" alt="YouTube Logo, once again, why are you reading this text.">
+        </br></br>
+        This will be all about me in a bit!! </br></br></br>
+        <img style="height: 40px;" src=".logos/.Discord-transparent.png" alt="Discord Logo, why are you reading this text.">
+        <a href="https://discord.gg/cPdSqvcXc9">My Discord Server!</a>  <! Hyperlink to my Discord Server ></br></br>
+	    <img style="height: 32px;" src=".logos/.Youtube-Transparent.png" alt="YouTube Logo, once again, why are you reading this text.">
         <a href="https://www.youtube.com/channel/UCZNRlvLcjqN8nw6YO73IPOg">My YouTube Channel!</a>  <! Hyperlink to my Youtube channel >
     </body>
 </html>

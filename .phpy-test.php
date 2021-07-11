051ce86392bc45c6baef2c0270fc27378edfc9a0
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 00:22:59 -0700

    Fixes my public IP, adds a meta viewport and fixes formatting in .phpy-test.php

--- .phpy-test.php
+++ .phpy-test.php
@@ -1,17 +1,18 @@
 <?php
-$link = "http://75.167.190.201";
+$link = "http://75.167.232.199";
 if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
 $command = escapeshellcmd('/var/www/html/NAS/send_script.py');
 $output = shell_exec($command);
 echo $output;
 ?><html>
     <head>
+      <body>
         <title>Welcome to my useful links!</title>
 	<link rel="icon" href="favicon.ico" type="image/x-icon"/>
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
 		<link href="/.stylesheet.css" rel="stylesheet" type="text/css">
+      </body>
     </head>
-    <body>
 </br>
     <a class="nav" href="<?php echo $link; ?>/Main-Page.php">Main Page</a>  <! Hyperlink back to the Main Page of the Web server >
 

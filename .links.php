4f5e39a27b515bbb068860ab3709411e28f19ef2
sanikdah <michaelfgarofalo@gmail.com>
2021-07-10 17:34:08 -0700

    Use better scaling code for the logos in .links.php

--- .links.php
+++ .links.php
@@ -12,31 +12,31 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
 	<a class="nav" href="<?php echo $link; ?>/Main-Page.php">Main Page</a>  <! Hyperlink back to the Main Page of the Web server >
 	<br>
 	<br>
-	<img style="height 0.1%;" src=".logos/.Google.png" alt="Google Logo">
+	<img style="height: 32px;" src=".logos/.Google.png" alt="Google Logo">
         <a href="https://www.google.com">Google</a>  <! Hyperlink to Google >
         <br>
         <br>
-	<img style="height 0.1%;" src=".logos/.YouTube.png" alt="YouTube Logo">
+	<img style="height: 32px;" src=".logos/.YouTube.png" alt="YouTube Logo">
         <a href="https://www.youtube.com">YouTube</a>  <! Hyperlink to Youtube >
         <br>
         <br>
-	<img style="height 0.1%;" src=".logos/.Google-Drive.png" alt="Google Drive Logo">
+	<img style="height: 32px;" src=".logos/.Google-Drive.png" alt="Google Drive Logo">
         <a href="https://drive.google.com">Google Drive</a>  <! Hyperlink to Google Drive >
 	<br>
 	<br>
-	<img style="height 0.1%;" src=".logos/.Wikipedia.ico" alt="Wikipedia Logo">
+	<img style="height: 32px;" src=".logos/.Wikipedia.ico" alt="Wikipedia Logo">
 	<a href="https://wikipedia.com">Wikipedia</a>  <! Hyperlink to Wikipedia >
 	<br>
 	<br>
-	<img style="height 0.1%;" src=".logos/.Yuzu-Emu.ico" alt="Yuzu Emulator Logo">
+	<img style="height: 32px;" src=".logos/.Yuzu-Emu.ico" alt="Yuzu Emulator Logo">
 	<a href="https://www.yuzu-emu.org/wiki/switch-mods/">Yuzu Mods</a>
 	<br>
 	<br>
-	<img style="height 0.1%;" src=".logos/.ROM-Patcher.png" alt="ROM Patcher Logo">
+	<img style="height: 32px;" src=".logos/.ROM-Patcher.png" alt="ROM Patcher Logo">
 	<a href="https://www.marcrobledo.com/RomPatcher.js/">Online ROM Patcher</a>
 	<br>
 	<br>
-	<img style="height 0.1%;" src=".logos/.SM64JS.png" alt="SM64JS Logo">
+	<img style="height: 32px;" src=".logos/.SM64JS.png" alt="SM64JS Logo">
 	<a href="https://sm64js.com">SM64JS</a>
     </body>
 </html>

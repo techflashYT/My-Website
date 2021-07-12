e7f181b7e470fc57c337a0ae91bbf266f40177a7
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 18:58:07 -0700

    Fixes comments and formatting on .help.php.

--- .help.php
+++ .help.php
@@ -7,21 +7,18 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://nas/";
         <title>Support</title>
     </head>
     <body>
-    <meta name=”viewport” content=”width=device-width, initial-scale=1″>
-        <br>
-        <a class="nav" href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a>  <! Hyperlink to Main-Page.php, the main page >
-        <br>
-        <br>
+    <meta name=”viewport” content=”width=device-width, initial-scale=1″></br>  <!-- Makes the page fit the device with, makes the website at least slightly more mobile friendly -->
+        <a class="nav" href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a></br></br>  <!-- Hyperlink to Main-Page.php, the main page -->
 	If something doesn't work correctly on the website, feel free to email me at michaelfgarofalo@gmail.com, preferably with screenshots of the issue. </br> If you want to suggest something, title your email "Suggestion for the website" or at least something simillar.
 	<input type="radio" name="Suggest/Help"> Suggestion
 	<input type="radio" name="Suggest/Help"> Support
 	<div><span>
 	 <form>
 <form method="post" action="send_script.php">
-  <input type="text" name="name" placeholder="Name" style="height: 30px"> </br> </br>
-  <input type="email" name="email" style="width: 250px; height: 30px" placeholder="Email"> </br> </br>
-  <input type="text" name="subject" placeholder="Subject" style="height: 30px"> </br> </br>
-  <textarea name="msg" style="height: 250px; width: 1000px" placeholder="Message"></textarea> </br> </br>
+  <input type="text" name="name" placeholder="Name" style="height: 30px"> </br></br>
+  <input type="email" name="email" style="width: 250px; height: 30px" placeholder="Email"> </br></br>
+  <input type="text" name="subject" placeholder="Subject" style="height: 30px"> </br></br>
+  <textarea name="msg" style="height: 250px; width: 1000px" placeholder="Message"></textarea> </br></br>
   <button type="submit" name="send_message_btn" style="background-color: DeepSkyBlue; height: 50px; width: 120px">Send</button>
 </form>
 </span></div>

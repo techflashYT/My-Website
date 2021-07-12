0ae2c0f02fa7ed5ff6e3444879e4587ee4fa2f80
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 18:55:51 -0700

    Fixes comments, and fixes formatting in .credits.php.

--- .credits.php
+++ .credits.php
@@ -7,11 +7,8 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://nas/";
         <title>Credits</title>
     </head>
     <body>
-    <meta name=”viewport” content=”width=device-width, initial-scale=1″>
-        <br>
-        <a class="nav" href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a>  <! Hyperlink to Main-Page.php, the main page >
-	<br>
-	<br>
+    <meta name=”viewport” content=”width=device-width, initial-scale=1″></br>  <!-- Makes the page fit the device with, makes the website at least slightly more mobile friendly -->
+        <a class="nav" href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a></br></br>  <!-- Hyperlink to Main-Page.php, the main page -->
 	Me, sanikdah: Creator, hoster
     </br></br>"BlueToonYoshi": Helped with the images on the "Useful Links" section.
     </br></br>"Void B⃢ot", "TheShadowEevee", "ping for so-so wii help | snare" "avane": helped a lot with the coding and setting up of the web server itself, a major thanks to these guys.

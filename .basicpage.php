1785226305ba955c9910cce2a96c1626d726d2a1
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 18:54:26 -0700

    Adds the get User Agent code, fixes formatting, and fixes comments on .basicpage.php.

--- .basicpage.php
+++ .basicpage.php
@@ -1,13 +1,16 @@
 <?php
+
+
+$browser = get_browser(null, true);
+print_r($browser);
+
 $link = "http://75.167.232.199";
-if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://nas/";
+if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
 ?><html>
     <head>
-        <link href="/.stylesheet.css" rel="stylesheet" type="text/css">
-        <title>Support</title>
+        <title>Welcome to the *change this to the page name of the page you are making*</title>
+		<link href=".stylesheet.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-    <meta name=”viewport” content=”width=device-width, initial-scale=1″>
-        <br>
-        <a class="nav" href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a>  <! Hyperlink to Main-Page.php, the main page >
-    </body>
+	<meta name=”viewport” content=”width=device-width, initial-scale=1″></br>  <!-- Makes the page fit the device with, makes the website at least slightly more mobile friendly -->
+		This is the *page name here* page and you are visiting from <?php echo $_SERVER['HTTP_USER_AGENT'] . "\n\n"; ?>

cc586bdabd7adc2a60f0ad4cfc34151be08d78b4
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 18:36:56 -0700

    Fix formatting and add code to display your user agent, not sure why I added this, ig just to flex.

--- Main-Page.php
+++ Main-Page.php
@@ -1,4 +1,9 @@
 <?php
+
+
+$browser = get_browser(null, true);
+print_r($browser);
+
 $link = "http://75.167.232.199";
 if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
 ?><html>
@@ -9,24 +14,16 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
     <body>
 	<meta name=”viewport” content=”width=device-width, initial-scale=1″>
 		<br>
-		<img style="height: 32px;" src=".logos/.Internet.png" alt="">
-	        <a href="<?php echo $link; ?>/.links.php">Useful Links</a>  <! Hyperlink to links.php, which has a lot of useful links on it >
-		<br>
-		<br>
+		This is the main page and you are visiting from <?php echo $_SERVER['HTTP_USER_AGENT'] . "\n\n"; ?>
+		</br></br><img style="height: 32px;" src=".logos/.Internet.png" alt="">
+	    <a href="<?php echo $link; ?>/.links.php">Useful Links</a>  <!-- Hyperlink to links.php, which has a lot of useful links on it -->
+		</br></br>
 		<img style="height: 32px;" src=".logos/.Game-Controller.png" alt="Game Controller icon">
-		<a href="<?php echo $link; ?>/.game.php">Bitcoiner Game</a>  <! Hyperlink to game.php, my Bitcoiner embed from crazygames >
-		<br>
-		<br>
-		<a href="<?php echo $link; ?>">Network Server</a>  <! Hyperlink to the website root >
-		<br>
-		<br>
-		<a href="<?php echo $link; ?>/.aboutme.php">Me</a>  <! Hyperlink to aboutme.php, a page about me >
-		<br>
-		<br>
-		<a href="<?php echo $link; ?>/.credits.php">Credits</a>  <! Hyperlink to credits.php, a list of everyone who helped make this site possible >
-		<br>
-		<br>
-		<a href="<?php echo $link; ?>/.help.php">Support/Suggestions</a>  <! Hyperlink to help.php, where users can suggest things or ask for support >
+		<a href="<?php echo $link; ?>/.game.php">Bitcoiner Game</a></br></br>  <!-- Hyperlink to game.php, my Bitcoiner embed from crazygames -->
+		<a href="<?php echo $link; ?>">Network Server</a></br></br>  <!-- Hyperlink to the website root -->
+		<a href="<?php echo $link; ?>/.aboutme.php">Me</a></br></br>  <!-- Hyperlink to aboutme.php, a page about me -->
+		<a href="<?php echo $link; ?>/.credits.php">Credits</a></br></br>  <!-- Hyperlink to credits.php, a list of everyone who helped make this site possible -->
+		<a href="<?php echo $link; ?>/.help.php">Support/Suggestions</a>  <!-- Hyperlink to help.php, where users can suggest things or ask for support -->
 	</body>
 </head>
 </html>

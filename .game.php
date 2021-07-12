a998ed113d53856fb63e49a771147c1cc6972a68
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 18:57:37 -0700

    Fixes comments and formatting on .game.php.

--- .game.php
+++ .game.php
@@ -8,9 +8,8 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
     </head>
     <body>
     <meta name=”viewport” content=”width=device-width, initial-scale=1″>  <!-- Makes the page fit the device with, makes the website at least slightly more mobile friendly -->
-        <a href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a>
-        <br>
-        <iframe width="1050" height="812" src="https://www.crazygames.com/embed/bitcoiner" frameborder="0" allowfullscreen></iframe>
+        <a href="<?php echo $link; ?>/Main-Page.php">Back to Main Page</a></br>  <!-- Hyperlink to Main-Page.php, the Main Page of the Web Server -->
+        <iframe width="1050" height="812" src="https://www.crazygames.com/embed/bitcoiner" frameborder="0" allowfullscreen></iframe>  <!-- iframe of the Bitcoiner game from Crazygames -->
         <?php echo "Bitcoiner is a game all about mining bitcoin!  </br> You start off with basic machines, no cooling and a small room. </br> As you get more bitcoins, you can upgrade all of those things!  </br> Can you get to 1 million bitcoin?"; ?>
     </body>
 </html>

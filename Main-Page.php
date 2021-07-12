91218cb9623bfeb14302708ae921c6a451260bfb
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 19:09:19 -0700

    Add an alt text to the internet globe icon, and add the server icon code to Main-Page.php.

--- Main-Page.php
+++ Main-Page.php
@@ -11,10 +11,11 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
     <body>
 	<meta name=”viewport” content=”width=device-width, initial-scale=1″>
 		</br>This is the main page and you are visiting from <?php echo $_SERVER['HTTP_USER_AGENT'] . "\n\n"; ?></br></br>
-		<img style="height: 32px;" src=".logos/.Internet.png" alt="">
+		<img style="height: 32px;" src=".logos/.Internet.png" alt="Internet globe icon">
 	    <a href="<?php echo $link; ?>/.links.php">Useful Links</a></br></br>  <!-- Hyperlink to links.php, which has a lot of useful links on it -->
 		<img style="height: 32px;" src=".logos/.Game-Controller.png" alt="Game Controller icon">
 		<a href="<?php echo $link; ?>/.game.php">Bitcoiner Game</a></br></br>  <!-- Hyperlink to game.php, my Bitcoiner embed from crazygames -->
+		<img style="height: 32px;" src=".logos/.Server.png" alt="Server Icon">
 		<a href="<?php echo $link; ?>">Network Server</a></br></br>  <!-- Hyperlink to the website root -->
 		<a href="<?php echo $link; ?>/.aboutme.php">Me</a></br></br>  <!-- Hyperlink to aboutme.php, a page about me -->
 		<a href="<?php echo $link; ?>/.credits.php">Credits</a></br></br>  <!-- Hyperlink to credits.php, a list of everyone who helped make this site possible -->

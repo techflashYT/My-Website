36db44475f339aa7d12a325228c125a680ec5e5f
sanikdah <michaelfgarofalo@gmail.com>
2021-07-11 19:07:04 -0700

    Fix some formatting isues in Main-Page.php.

--- Main-Page.php
+++ Main-Page.php
@@ -10,11 +10,9 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS";
     </head>
     <body>
 	<meta name=”viewport” content=”width=device-width, initial-scale=1″>
-		<br>
-		This is the main page and you are visiting from <?php echo $_SERVER['HTTP_USER_AGENT'] . "\n\n"; ?>
-		</br></br><img style="height: 32px;" src=".logos/.Internet.png" alt="">
-	    <a href="<?php echo $link; ?>/.links.php">Useful Links</a>  <!-- Hyperlink to links.php, which has a lot of useful links on it -->
-		</br></br>
+		</br>This is the main page and you are visiting from <?php echo $_SERVER['HTTP_USER_AGENT'] . "\n\n"; ?></br></br>
+		<img style="height: 32px;" src=".logos/.Internet.png" alt="">
+	    <a href="<?php echo $link; ?>/.links.php">Useful Links</a></br></br>  <!-- Hyperlink to links.php, which has a lot of useful links on it -->
 		<img style="height: 32px;" src=".logos/.Game-Controller.png" alt="Game Controller icon">
 		<a href="<?php echo $link; ?>/.game.php">Bitcoiner Game</a></br></br>  <!-- Hyperlink to game.php, my Bitcoiner embed from crazygames -->
 		<a href="<?php echo $link; ?>">Network Server</a></br></br>  <!-- Hyperlink to the website root -->

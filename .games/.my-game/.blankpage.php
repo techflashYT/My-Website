5190907f62340b4c5f61fa49c25019eb5d8d74c9
sanikdah <michaelfgarofalo@gmail.com>
2021-07-13 22:01:12 -0700

    Update .blankpage.php
    Added the game to here.

--- .games/.my-game/.blankpage.php
+++ .games/.my-game/.blankpage.php
@@ -7,8 +7,18 @@ if (substr($_SERVER["REMOTE_ADDR"], 0, 8) === "192.168.") $link = "http://NAS/";
 
 <head>
     <title>Welcome to the *change this to the page name of the page you are making*</title>
-    <link href=".stylesheet-blank.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo $link ?>.stylesheet-blank.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <meta name=”viewport” content=”width=device-width, initial-scale=1″></br> <!-- Makes the page fit the device with, makes the website at least slightly more mobile friendly -->
+    <script type="text/javascript" src="<?php echo $link; ?>.games\.my-game\.js\.my-game.js">
+    </script>
+    <script type="text/javascript" src="<?php echo $link; ?>.games\.my-game\.js\p5.js">
+    </script>
+    <script type="text/javascript" src="<?php echo $link; ?>.games\.my-game\.js\sketch.js">
+    </script>
+    <p id="test" />
+    <script>
+        document.getElementById("test").innerHTML = '';
+    </script>

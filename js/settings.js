cf66ff061b4e36486a4d04f932c133fcd9665018
Techflash <72118300+techflashYT@users.noreply.github.com>
2022-03-25 18:50:44 -0700

    LF => CRLF

--- js/settings.js
+++ js/settings.js
@@ -1,14 +1,17 @@
-function save(settingName, settingValue) {
-    try {
-        window.localStorage.setItem(settingName, settingValue);
-    }
-    catch (err) {
-        // eval("alert(\"An error has occurred trying to save a setting (" + settingName + ") with value \"" + settingValue + "\"!  The error is \"" + err + "\"");
-        return 1;
-    }
-    return 0;
-}
+class settings {
 
-function load(settingName) {
-    return window.localStorage.getItem(settingName);
+    save(settingName, settingValue) {
+        try {
+            window.localStorage.setItem(settingName, settingValue);
+        }
+        catch (err) {
+            // eval("alert('An error has occurred trying to save a setting (\' + settingName + \') with value \" + settingValue + '!  The error is ' + err + '\''");
+            return 1;
+        }
+        return 0;
+    }
+
+    load(settingName) {
+        return window.localStorage.getItem(settingName);
+    }
 }

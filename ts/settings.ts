e085428f8490b1d17f007cadab6610c4a0fddd0b
Techflash <michaelfgarofalo@gmail.com>
2022-04-25 20:12:43 -0700

    Ig "string" is better than "String"

--- ts/settings.ts
+++ ts/settings.ts
@@ -1,5 +1,5 @@
 class settings {
-    save(settingName: String, settingValue: any) {
+    save(settingName: string, settingValue: any) {
         try {
             window.localStorage.setItem(settingName, settingValue);
         }
@@ -10,7 +10,7 @@ class settings {
         return 0;
     }
 
-    load(settingName: String) {
+    load(settingName: string) {
         return window.localStorage.getItem(settingName);
     }
 }

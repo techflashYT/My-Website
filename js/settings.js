669ee1fd95d0d50c0274c270464c387c230d8262
Techflash <michaelfgarofalo@gmail.com>
2022-04-29 20:14:46 -0700

    Modified TSC to use tabs, lowering storage

--- js/settings.js
+++ js/settings.js
@@ -1,17 +1,17 @@
 var settings = (function () {
-    function settings() {
-    }
-    settings.prototype.save = function (settingName, settingValue) {
-        try {
-            window.localStorage.setItem(settingName, settingValue);
-        }
-        catch (err) {
-            return 1;
-        }
-        return 0;
-    };
-    settings.prototype.load = function (settingName) {
-        return window.localStorage.getItem(settingName);
-    };
-    return settings;
+	function settings() {
+	}
+	settings.prototype.save = function (settingName, settingValue) {
+		try {
+			window.localStorage.setItem(settingName, settingValue);
+		}
+		catch (err) {
+			return 1;
+		}
+		return 0;
+	};
+	settings.prototype.load = function (settingName) {
+		return window.localStorage.getItem(settingName);
+	};
+	return settings;
 }());
